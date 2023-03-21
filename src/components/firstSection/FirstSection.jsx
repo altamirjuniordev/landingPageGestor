8e02b3cf4a171a25f6560e2d3bd79b8f46e09ee2
Altamir Junior <altamirjuniordev@hotmail.com>
2023-03-21 10:56:56 -0300

    ajustes css, firstSection e Services

--- src/components/firstSection/FirstSection.jsx
+++ src/components/firstSection/FirstSection.jsx
@@ -3,7 +3,7 @@ import './firstSection.css'
 
 const FirstSection = () => {
   return (
-    <section class="firstSection d-sm-flex py-4 justify-content-center gap-5">
+    <section class="firstSection d-sm-flex py-5 justify-content-center gap-5">
       <div class="card bg-dark m-sm-0 mx-auto mb-5" style="max-width: 18rem;">
         <div class="text-center">
           <i class="bi bi-person-fill-add fs-1" style="color: #ff8311;"></i>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -4,14 +4,14 @@ import img from '../../assets/headerImg.png'
 
 const Services = () => {
   return (
-    <section className="p-2">
+    <section className="p-5">
       <h2 className="text-center">NOSSOS SERVIÇOS</h2>
       <p className="text-center">UM POUCO DOS SERVIÇOS QUE PRESTAMOS</p>
 
       {/* CARDS */}
 
-      <div className="d-flex justify-content-around flex-wrap ">
-        <div class="card my-3" style="max-width: 18rem;">
+      <div className="d-flex justify-content-center  flex-wrap gap-3 ">
+        <div class="card " style="max-width: 18rem;">
           <img src={img} className='img-fluid' alt="" />
           <div class="card-body">
             <p class="card-text">
@@ -19,7 +19,7 @@ const Services = () => {
             </p>
           </div>
         </div>
-        <div class="card my-3" style="max-width: 18rem;">
+        <div class="card " style="max-width: 18rem;">
         <img src={img} className='img-fluid' alt="" />
           <div class="card-body">
             <p class="card-text">
@@ -27,7 +27,7 @@ const Services = () => {
             </p>
           </div>
         </div>
-        <div class="card my-3" style="max-width: 18rem;">
+        <div class="card" style="max-width: 18rem;">
         <img src={img} className='img-fluid' alt="" />
           <div class="card-body">
             <p class="card-text">
